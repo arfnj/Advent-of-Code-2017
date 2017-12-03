0cec592afe74dffbbf254a2b5cf218e99b020532
Andrew Fechner <arfechner@gmail.com>
2017-12-02 22:22:43 -0500

    Solves day 2

--- 01_Inverse-Captcha.js
+++ 01_Inverse-Captcha.js
@@ -1,12 +1,19 @@
-// You're standing in a room with "digitization quarantine" written in LEDs along one wall. The only door is locked, but it includes a small interface. "Restricted Area - Strictly No Digitized Users Allowed."
+// You're standing in a room with "digitization quarantine" written in LEDs along one wall. The only
+// door is locked, but it includes a small interface. "Restricted Area - Strictly No Digitized Users
+// Allowed."
 
-// It goes on to explain that you may only leave by solving a captcha to prove you're not a human. Apparently, you only get one millisecond to solve the captcha: too fast for a normal human, but it feels like hours to you.
+// It goes on to explain that you may only leave by solving a captcha to prove you're not a human.
+// Apparently, you only get one millisecond to solve the captcha: too fast for a normal human, but
+// it feels like hours to you.
 
-// The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
+// The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of
+// all digits that match the next digit in the list. The list is circular, so the digit after the
+// last digit is the first digit in the list.
 
 // For example:
 
-// 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+// 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the
+// third digit (2) matches the fourth digit.
 // 1111 produces 4 because each digit (all 1) matches the next.
 // 1234 produces 0 because no digit matches the next.
 // 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
@@ -22,15 +29,19 @@ const input = '42812249899758728399611695139795793356913694984837941712536253226
 			sum += Number(val)
 		}
 	});
+	console.log(input);
 	return arr[0] === arr[arr.length-1] ? sum + Number(arr[0]) : sum;
 })(input);
 
 
 // --- Part Two ---
 
-// You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied, but it did emit a star as encouragement. The instructions change:
+// You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied,
+// but it did emit a star as encouragement. The instructions change:
 
-// Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list. That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
+// Now, instead of considering the next digit, it wants you to consider the digit halfway around the
+// circular list. That is, if your list contains 10 items, only include a digit in your sum if the
+// digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
 
 // For example:
 
@@ -45,7 +56,6 @@ const input = '42812249899758728399611695139795793356913694984837941712536253226
 	const arr = input.split('');
 	const len = arr.length;
 	const half = len/2;
-	console.log(len,half);
 	let sum = 0;
 	for (let i=0; i<len; i++) {
 		if (arr[i] === arr[(i+half)%len]) {
