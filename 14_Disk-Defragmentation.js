006fdc12a3ffff774fad2c8be68e3c64acf70bd7
Andrew Fechner <arfechner@gmail.com>
2017-12-20 11:37:45 -0500

    Solves Day 14

--- 14_Disk-Defragmentation.js
+++ 14_Disk-Defragmentation.js
@@ -41,6 +41,7 @@
 // Your puzzle input is ffayrhll.
 
 const input = 'ffayrhll-';
+const grid = [];
 
 const reverser = (list,startIndex,count) => {
 	let left = startIndex;
@@ -84,6 +85,7 @@ const knotMaker = (input) => {
 (() => {
 	let ones = 0;
 	for (let i=0; i<128; i++) {
+		let row = [];
 		let hashInput = input + i;
 		const hash = knotMaker(hashInput);
 		for (let bit of hash) {
@@ -91,8 +93,71 @@ const knotMaker = (input) => {
 			let bin = dec.toString(2).split('');
 			bin.unshift('0','0','0');
 			bin = bin.slice(-4);
+			row = row.concat(bin);
 			ones += bin.filter(bit => bit === '1').length;
 		}
+		grid.push(row);
 	}
-	console.log(ones);
+	console.log(`${ones} 1's`);
 })();
+
+
+// Now, all the defragmenter needs to know is the number of regions. A region is a group of used
+// squares that are all adjacent, not including diagonals. Every used square is in exactly one
+// region: lone used squares form their own isolated regions, while several adjacent squares all
+// count as a single region.
+
+// In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+// 11.2.3..-->
+// .1.2.3.4   
+// ....5.6.   
+// 7.8.55.9   
+// .88.5...   
+// 88..5..8   
+// .8...8..   
+// 88.8.88.-->
+// |      |   
+// V      V   
+
+// Of particular interest is the region marked 8; while it does not appear contiguous in this small
+// view, all of the squares marked 8 are connected when considering the whole 128x128 grid. In
+// total, in this example, 1242 regions are present.
+
+// How many regions are present given your key string?
+
+const regionFinder = (grid) => {
+	let playground = [...grid];
+	let regions = 0;
+	let rows = playground.length;
+	let cols = playground[0].length;
+
+	const seekAndDestroy = (r,c) => {
+		playground[r][c] = '0';
+		if (r>0 && playground[r-1][c] === '1') {
+			seekAndDestroy(r-1,c);
+		}
+		if (r<(rows-1) && playground[r+1][c] === '1') {
+			seekAndDestroy(r+1,c);
+		}
+		if (c>0 && playground[r][c-1] === '1') {
+			seekAndDestroy(r,c-1);
+		}
+		if (c<(cols-1) && playground[r][c+1] === '1') {
+			seekAndDestroy(r,c+1);
+		}
+	}
+
+	for (let row=0; row<rows; row++) {
+		for (let col=0; col<cols; col++) {
+			if (playground[row][col] === '1') {
+				seekAndDestroy(row,col);
+				regions++;
+			}
+		}
+	}
+	console.log(`${regions} regions`)
+}
+
+regionFinder(grid);
+
